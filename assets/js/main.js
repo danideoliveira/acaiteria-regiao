36a6fa0f65953182cc34a6d5a3ba32939dafa478
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2022-06-24 19:51:59 -0300

    Aplicando responsividade

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,20 @@
-const imgHomeIceCream = document.querySelector('.home-ice-cream');
-const homeImgDiv = document.querySelector('.home-image-div');
-const locationDivMap = document.querySelector('.location-div-map');
+function iceCreamHomeAnimation() {
+    const imgHomeIceCream = document.querySelector('.home-ice-cream');
+    const homeImgDiv = document.querySelector('.home-image-div');
 
-const BORDER_RADIUS_MOUSE_OUT = `${260}px ${10}px ${10}px ${10}px`;
-const BORDER_RADIUS_MOUSE_ENTER = `${10}px ${10}px ${10}px ${10}px`;
-const TRANSITION_VALUE = `${0.5}s`;
+    const BORDER_RADIUS_MOUSE_OUT = `${260}px ${10}px ${10}px ${10}px`;
+    const BORDER_RADIUS_MOUSE_ENTER = `${10}px ${10}px ${10}px ${10}px`;
+    const TRANSITION_VALUE = `${0.5}s`;
 
-imgHomeIceCream.addEventListener('mouseenter', (e) => {
-    homeImgDiv.style.borderRadius = BORDER_RADIUS_MOUSE_ENTER;
-    homeImgDiv.style.transition = TRANSITION_VALUE;
-})
+    imgHomeIceCream.addEventListener('mouseenter', (e) => {
+        homeImgDiv.style.borderRadius = BORDER_RADIUS_MOUSE_ENTER;
+        homeImgDiv.style.transition = TRANSITION_VALUE;
+    })
 
-imgHomeIceCream.addEventListener('mouseout', (e) => {
-    homeImgDiv.style.borderRadius = BORDER_RADIUS_MOUSE_OUT;
-    homeImgDiv.style.transition = TRANSITION_VALUE;
-})
+    imgHomeIceCream.addEventListener('mouseout', (e) => {
+        homeImgDiv.style.borderRadius = BORDER_RADIUS_MOUSE_OUT;
+        homeImgDiv.style.transition = TRANSITION_VALUE;
+    })
+}
+
+iceCreamHomeAnimation();
