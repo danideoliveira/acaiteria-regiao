61f022be3f426d511f1e857b08df3837587accc5
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2022-11-25 15:36:23 -0300

    Add scroll reveal

--- src/components/card-item/CardItem.jsx
+++ src/components/card-item/CardItem.jsx
@@ -1,26 +1,30 @@
 // import "./CardItem.scss";
 import { Container, DivSpecialtiesInfo, Title, Description, Price, Button, DivImage } from './styled';
+import { motion } from 'framer-motion';
+import {Fade} from 'react-reveal'
 
-export default function CardItem({title, description, image, value, setButton, alt}) {
-    
+export default function CardItem({ title, description, image, value, setButton, alt }) {
+
     function setButtonValidate(result) {
-        if(result) {
+        if (result) {
             return <Button>Monte seu copo!</Button>
         }
     }
 
     return (
-        <Container>
-            <DivImage>
-                <img src={ image } alt={ alt } />
-            </DivImage>
-            <DivSpecialtiesInfo>
-                <Title>{ title }</Title>
-                <Description>{ description }</Description>
-                <Price>{ value }</Price>       
-                {setButtonValidate(setButton)}      
-            </DivSpecialtiesInfo>          
-        </Container>
+        <Fade bottom>
+            <Container>
+                <DivImage>
+                    <img src={image} alt={alt} />
+                </DivImage>
+                <DivSpecialtiesInfo>
+                    <Title>{title}</Title>
+                    <Description>{description}</Description>
+                    <Price>{value}</Price>
+                    {setButtonValidate(setButton)}
+                </DivSpecialtiesInfo>
+            </Container>
+        </Fade>
     );
 }
 
