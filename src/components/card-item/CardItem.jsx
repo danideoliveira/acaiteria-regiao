e96e7d59669bcb7adba70530036c8192a94a7c87
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2022-11-25 15:57:10 -0300

    Remove scroll reveal

--- src/components/card-item/CardItem.jsx
+++ src/components/card-item/CardItem.jsx
@@ -1,7 +1,6 @@
 // import "./CardItem.scss";
 import { Container, DivSpecialtiesInfo, Title, Description, Price, Button, DivImage } from './styled';
 import { motion } from 'framer-motion';
-import {Fade} from 'react-reveal'
 
 export default function CardItem({ title, description, image, value, setButton, alt }) {
 
@@ -12,19 +11,17 @@ export default function CardItem({ title, description, image, value, setButton, 
     }
 
     return (
-        <Fade bottom>
-            <Container>
-                <DivImage>
-                    <img src={image} alt={alt} />
-                </DivImage>
-                <DivSpecialtiesInfo>
-                    <Title>{title}</Title>
-                    <Description>{description}</Description>
-                    <Price>{value}</Price>
-                    {setButtonValidate(setButton)}
-                </DivSpecialtiesInfo>
-            </Container>
-        </Fade>
+        <Container>
+            <DivImage>
+                <img src={image} alt={alt} />
+            </DivImage>
+            <DivSpecialtiesInfo>
+                <Title>{title}</Title>
+                <Description>{description}</Description>
+                <Price>{value}</Price>
+                {setButtonValidate(setButton)}
+            </DivSpecialtiesInfo>
+        </Container>
     );
 }
 
