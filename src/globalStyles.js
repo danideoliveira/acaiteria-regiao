3c61a0cff8adcfa2559f2010d1876677a435e085
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2022-11-26 15:59:00 -0300

    Add global styles

---
+++ src/globalStyles.js
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@300;400;500;600;700;800&display=swap');
+    @import url('http://fonts.cdnfonts.com/css/sunshine-festival');
+    @import url('http://fonts.cdnfonts.com/css/healing-lighters');
+    @import url('http://fonts.cdnfonts.com/css/beach-sound');
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+        font-family: 'League Spartan', Arial, Helvetica, sans-serif;
+        text-decoration: none;
+        scroll-behavior: smooth;
+        border: none;
+    }
+
+    html {
+        font-size: 62.5%;
+    }
+
+    body {
+        overflow-x: hidden;
+    }
+
+    @media (min-width: 320px) and (max-width: 768px) {
+        html {
+            font-size: 60%;
+        }
+    }
+
+    @media (min-width: 320px) and (max-width: 480px) {
+        html {
+            font-size: 57%;
+        }
+    }
+`;
