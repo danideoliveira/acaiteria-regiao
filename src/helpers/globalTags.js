53948d076c024bf7ac18e9fddc6fa1be9cada10d
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2022-11-26 20:26:06 -0300

    Add global tags

---
+++ src/helpers/globalTags.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Title = styled.h2`
+    font-family: "Beach Sound", Arial, Helvetica, sans-serif;
+    width: 100%;
+    font-size: 5.4rem;
+    color: ${(props) => props.color};
+
+    @media (min-width: 320px) and (max-width: 900px) {
+        text-align: center;
+    }
+`;
+
+export const Description = styled.p`
+    line-height: 2.5rem;
+    width: 100%;
+    text-align: justify;
+    font-size: 2rem;
+    color: ${(props) => props.color};
+`;
