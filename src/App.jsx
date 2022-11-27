4a180ce032be30bf205dc0c6312213ebe7a3b9ad
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2022-11-26 21:15:26 -0300

    Fix path bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Navbar from './components/navbar/Navbar';
-import Home from './components/Home/Home';
-import Specialties from './components/Specialties/Specialties';
-import Location from './components/Location/Location';
-import About from './components/About/About';
-import Footer from './components/Footer/Footer';
+import Home from './components/home/Home';
+import Specialties from './components/specialties/Specialties';
+import Location from './components/location/Location';
+import About from './components/about/About';
+import Footer from './components/footer/Footer';
 import { GlobalStyle } from './globalStyles';
 
 function App() {
