1641e6bbe1cd4df83141f848c06d014029fffd7d
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2022-11-30 19:47:51 -0300

    Update font family

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './Fonts.css'
 import Navbar from './components/navbar/Navbar';
 import Home from './components/home/Home';
 import Specialties from './components/specialties/Specialties';
--- src/globalStyles.js
+++ src/globalStyles.js
@@ -2,9 +2,6 @@ import { createGlobalStyle } from "styled-components";
 import { colors } from "./helpers/variables";
 
 export const GlobalStyle = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@300;400;500;600;700;800&display=swap');
-    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&display=swap');
-
     * {
         margin: 0;
         padding: 0;
@@ -14,15 +11,15 @@ export const GlobalStyle = createGlobalStyle`
         scroll-behavior: smooth;
         border: none;
     }
-
+    
     html {
         font-size: 62.5%;
     }
 
     body {
+        font-family: 'League Spartan', Arial, Helvetica, sans-serif;
         overflow-x: hidden;
         background-color: ${colors.cream};
-        font-family: "League Spartan", Arial, Helvetica, sans-serif;
     }
 
     @media (min-width: 320px) and (max-width: 768px) {
