b250311a40240056d044a4cc310afa3707efe1f5
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2022-12-01 20:11:18 -0300

    Add animated routes

---
+++ src/helpers/AnimatedRoutes.jsx
@@ -0,0 +1,25 @@
+import Home from "../components/home/Home"
+import Specialties from '../components/specialties/Specialties';
+import Location from '../components/location/Location';
+import About from '../components/about/About';
+import {
+    Route,
+    Routes,
+    useLocation
+} from "react-router-dom";
+
+import {AnimatePresence} from 'framer-motion';
+
+export default function AnimatedRoutes() {
+    const location = useLocation();
+    return (
+        <AnimatePresence>
+            <Routes location={location} key={location.pathname}>
+                <Route path='/' element={<Home />}/>
+                <Route path='/especialidades' element={<Specialties />}/>
+                <Route path='/localizacao' element={<Location />}/>
+                <Route path='/sobre' element={<About />}/>
+            </Routes>
+        </AnimatePresence>
+    )
+};
