84d60939a89cfd041fad467e19cc4c32b66fcbfa
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2022-12-04 21:38:01 -0300

    Add CEP api

--- src/components/modal-info/ModalInfo.jsx
+++ src/components/modal-info/ModalInfo.jsx
@@ -3,8 +3,29 @@ import { colors } from '../../helpers/variables';
 import { motion } from 'framer-motion';
 import { Modal, Container, ContainerSecondary, CloseButton, FormInfo, Button } from './ModalInfo.styled';
 import { showModal } from '../../helpers/Mixins';
+import getAddress from '../../modules/getAddress';
 
 export default function ModalInfo() {
+    async function showAddress(cep) {
+        const inputEndereco = document.querySelector('.endereco');
+        const inputBairro = document.querySelector('.bairro');
+        const inputCidade = document.querySelector('.cidade');
+
+        const dados = await getAddress(cep);
+        if(!dados || dados.bairro === 'undefined') {
+            inputEndereco.value = '';
+            inputBairro.value = '';
+            inputCidade.value = '';
+            return;
+        };
+        
+        const { logradouro, bairro, localidade  } = dados;
+
+        inputEndereco.value = logradouro;
+        inputBairro.value = bairro;
+        inputCidade.value = localidade;
+    }
+
     return(
         <motion.div
             initial={{opacity: 0}}
@@ -38,11 +59,11 @@ export default function ModalInfo() {
                                         <legend>Endereço</legend>
                                         <div>
                                             <label>CEP</label>
-                                            <input type='text' placeholder='Digite seu CEP...'></input>
+                                            <input type='text' placeholder='Digite seu CEP...' onChange={(e) => showAddress(e.target.value)}></input>
                                         </div>
                                         <div>
                                             <label>Endereço</label>
-                                            <input type='text'></input>
+                                            <input type='text' className='endereco'></input>
                                         </div>
                                         <div>
                                             <label>Número</label>
@@ -50,11 +71,11 @@ export default function ModalInfo() {
                                         </div>
                                         <div>
                                             <label>Bairro</label>
-                                            <input type='text'></input>
+                                            <input type='text' className='bairro'></input>
                                         </div>
                                         <div>
                                             <label>Cidade</label>
-                                            <input type='text'></input>
+                                            <input type='text' className='cidade'></input>
                                         </div>
                                     </fieldset>
 
---
+++ src/modules/getAddress.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export default async function getAddress(cep) {
+    try {
+        const cepReplaced = cep.replace(/[^0-9]/, "");
+		if(cepReplaced.length != 8) return;
+
+        const address = await axios(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(dados => dados)
+        .then(dados => dados.data);
+
+        return address;
+
+    } catch(e) {
+        return e;
+    }
+}
