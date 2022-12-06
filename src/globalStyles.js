f98ebccd19d830a7e6a7687464b105146594fccc
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2022-12-05 21:32:11 -0300

    Update code style

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -2,35 +2,35 @@ import { createGlobalStyle } from "styled-components";
 import { colors } from "./helpers/variables";
 
 export const GlobalStyle = createGlobalStyle`
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-        text-decoration: none;
-        scroll-behavior: smooth;
-        border: none;
-        font-family: 'League Spartan', Arial, Helvetica, sans-serif;
-    }
-    
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    list-style: none;
+    text-decoration: none;
+    scroll-behavior: smooth;
+    border: none;
+    font-family: 'League Spartan', Arial, Helvetica, sans-serif;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    overflow-x: hidden;
+    background-color: ${colors.cream};
+  }
+
+  @media (min-width: 320px) and (max-width: 768px) {
     html {
-        font-size: 62.5%;
+      font-size: 60%;
     }
+  }
 
-    body {
-        overflow-x: hidden;
-        background-color: ${colors.cream};
+  @media (min-width: 320px) and (max-width: 480px) {
+    html {
+      font-size: 50%;
     }
-
-    @media (min-width: 320px) and (max-width: 768px) {
-        html {
-            font-size: 60%;
-        }
-    }
-
-    @media (min-width: 320px) and (max-width: 480px) {
-        html {
-            font-size: 50%;
-        }
-    }
+  }
 `;
