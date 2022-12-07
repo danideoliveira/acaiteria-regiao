f15a6af31e63d14449c263186fc3c2dc9d25c15e
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2022-12-07 14:38:27 -0300

    Update axios response

--- src/modules/getAddress.js
+++ src/modules/getAddress.js
@@ -3,11 +3,11 @@ import axios from "axios";
 export default async function getAddress(cep) {
   try {
     const cepReplaced = cep.replace(/[^0-9]/, "");
-    if (cepReplaced.length != 8) return;
+    if (cepReplaced.length !== 8) return;
 
     const address = await axios(`https://viacep.com.br/ws/${cep}/json/`)
-      .then((dados) => dados)
-      .then((dados) => dados.data);
+      .then(json => json)
+      .then(response => response.data);
 
     return address;
   } catch (e) {
